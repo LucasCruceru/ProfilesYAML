b2e2258c721c2a15b3d916e38cdb40a27f1fd1ff
Lucas Cruceru <lucascruceru@gmail.com>
2017-07-04 22:37:43 +0200

    one more error to fix and its ready

--- src/main/java/ro/fortech/Application.java
+++ src/main/java/ro/fortech/Application.java
@@ -1,4 +1,24 @@
 package ro.fortech;
 
-public class Application {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ro.fortech.config.ServerProperties;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private ServerProperties properties;
+
+    @Override
+    public void run(String... args) throws Exception{
+        System.out.println(properties);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
